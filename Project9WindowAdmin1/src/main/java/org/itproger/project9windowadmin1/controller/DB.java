dca59bc677468a2a71e8268e0e6664939b79cbe7
Oleg <myacckde@gmail.com>
2026-08-15 16:43:02 +0300

    Fix DB connection string to use lowercase jdbc and add common options

--- Project9WindowAdmin1/src/main/java/org/itproger/project9windowadmin1/controller/DB.java
+++ Project9WindowAdmin1/src/main/java/org/itproger/project9windowadmin1/controller/DB.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 
 public class DB {
 
-
     private static final String HOST = "localhost";
     private static final String PORT = "3306";
     private static final String DB_NAME = "hibernate";
@@ -14,21 +13,18 @@ public class DB {
     private static final String PASS = "root";
 
     public static Connection dbConn = null;
+
     public static Connection getDbConnection() throws ClassNotFoundException, SQLException {
-        String connStr = "JDBC:mysql://"+HOST +":"+PORT+"/"+DB_NAME;
+        String connStr = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB_NAME + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         DB.dbConn = DriverManager.getConnection(connStr, LOGIN, PASS);
         return dbConn;
     }
+
     public void isConnected() throws SQLException, ClassNotFoundException {
         dbConn = getDbConnection();
         System.out.println(dbConn.isValid(1000));
     }
-   /* public boolean isExistsUser(String login){
 
-        return false;
-    };
-*/
-
-  }
+}
